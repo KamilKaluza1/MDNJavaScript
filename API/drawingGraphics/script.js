ec6406f942e6c432f54507ac201eee4301437621
Kamil Kałuża <kamil321.94@gmail.com>
2022-11-04 09:03:25 +0100

    d

--- API/drawingGraphics/script.js
+++ API/drawingGraphics/script.js
@@ -4,26 +4,37 @@ const height = canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
 
-function degToRad(degrees) {
-    return degrees * Math.PI / 180;
-}
+// function degToRad(degrees) {
+//     return degrees * Math.PI / 180;
+// }
 
-ctx.fillStyle = "rgb(255, 0, 0)";
-ctx.beginPath();
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-const triHeight = 50 * Math.tan(degToRad(60));
-ctx.lineTo(100, 50 + triHeight);
-ctx.lineTo(50, 50);
-ctx.fill();
+// ctx.fillStyle = "rgb(255, 0, 0)";
+// ctx.beginPath();
+// ctx.moveTo(50, 50);
+// ctx.lineTo(150, 50);
+// const triHeight = 50 * Math.tan(degToRad(60));
+// ctx.lineTo(100, 50 + triHeight);
+// ctx.lineTo(50, 50);
+// ctx.fill();
 
-ctx.fillStyle = "rgb(0, 0, 255)"
-ctx.beginPath();
-ctx.arc(150,106,50, degToRad(0), degToRad(360),false);
-ctx.fill();
+// ctx.fillStyle = "rgb(0, 0, 255)"
+// ctx.beginPath();
+// ctx.arc(150,106,50, degToRad(0), degToRad(360),false);
+// ctx.fill();
 
-ctx.fillStyle = "yellow";
-ctx.beginPath();
-ctx.arc(200, 106, 50, degToRad(-45), degToRad(45), true);
-ctx.lineTo(200,106);
-ctx.fill()
+// ctx.fillStyle = "yellow";
+// ctx.beginPath();
+// ctx.arc(200, 106, 50, degToRad(-45), degToRad(45), true);
+// ctx.lineTo(200,106);
+// ctx.fill()
+
+ctx.strokeStyle = "black";
+ctx.lineWidth = 1;
+ctx.font = "36px arial";
+ctx.strokeText("Canvas text", 50, 50);
+
+ctx.fillStyle = "red";
+ctx.font = "48px georgia";
+ctx.fillText("inny text", 50, 150);
+
+canvas.setAttribute("aria-label", "inny text")
