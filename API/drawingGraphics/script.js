f8d7b36cadc2cdec180091144afa6efcb196a8d2
Kamil Kałuża <kamil321.94@gmail.com>
2022-11-04 13:25:01 +0100

    d

--- API/drawingGraphics/script.js
+++ API/drawingGraphics/script.js
@@ -3,6 +3,8 @@ const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
+ctx.fillStyle = "rgb(0, 0, 0)";
+ctx.fillRect(0, 0, width, height);
 
 // function degToRad(degrees) {
 //     return degrees * Math.PI / 180;
@@ -47,7 +49,35 @@ const ctx = canvas.getContext('2d');
 // image.src = "firefox.png";
 // image.addEventListener("load", () => ctx.drawImage(image, 20, 20, 185, 175));
 
-// --------------------animations--------------
+// -------------------- loop --------------
 
+// ctx.translate(width / 2, height / 2);
 
+// function degToRad(degrees) {
+//     return degrees * Math.PI /180;
+// }
 
+// function rand(min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// let length = 125;
+// let moveOffset = 0;
+
+// for (let i = 0; i < length; i++) {
+//     ctx.fillStyle = `rgba(${255 - length},0,${255 - length}, 0.5)`;
+//     ctx.beginPath();
+//     ctx.moveTo(moveOffset, moveOffset);
+//     ctx.lineTo(moveOffset + length, moveOffset);
+//     const triHeight = length / 2 * Math.tan(degToRad(60));
+//     ctx.lineTo(moveOffset + length / 2, moveOffset + triHeight);
+//     ctx.lineTo(moveOffset, moveOffset);
+//     ctx.fill();
+
+//     length--;
+//     moveOffset += 1;
+//     ctx.rotate(degToRad(5));
+
+// }
+
+// -------------------- animation --------------
