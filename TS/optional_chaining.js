0e3ada4f15aa6976bcf9f540a26fc4e7a22b1051
Kamil Kałuża <kamil321.94@gmail.com>
2022-11-17 13:43:33 +0100

    d

---
+++ TS/optional_chaining.js
@@ -0,0 +1,16 @@
+const personA = {
+    name: 'John',
+    address: {
+        city:'Winterfell'
+    },
+    hobbies: [
+        { title: 'horse riding' },
+        { title: 'sword fight' }
+    ],
+    fight: () => console.log('I will fight you!')
+}
+
+const personB = {
+    name: 'Sans',
+    hobbies: []
+}
